7045c6625d62168f2825cf09b789d37439db7592
enricode <145587481+ScarXiFy@users.noreply.github.com>
2024-12-05 09:42:43 +0800

    Update MusicArray.java

--- Final Exam/MusicArray.java
+++ Final Exam/MusicArray.java
@@ -3,24 +3,26 @@ import java.util.ArrayList;
 
 public class MusicArray
 {
-	private ArrayList<Music> musics = new ArrayList<>();
+	private ArrayList<Music> musics = new ArrayList<>(10);
 	
 	MusicArray()
 	{
-		musics.add(new Pop("Uptown Funk"));
-		musics.add(new Pop("Shape of You"));
-		musics.add(new Pop("Dance of the Night"));
-		musics.add(new Rock("Blinding Lights"));
-		musics.add(new Rock("Beat it"));
-		musics.add(new Rock("Back in Black"));
-		musics.add(new Rap("DNA"));
-		musics.add(new Rap("Count Me Out"));
-		musics.add(new Rap("FEIN"));
+		musics.add(new Pop("POP MUSIC #1"));
+		musics.add(new Pop("POP MUSIC #2"));
+		musics.add(new Pop("POP MUSIC #3"));
+		musics.add(new Pop("POP MUSIC #4"));
+		musics.add(new Pop("POP MUSIC #5"));
+		
+		musics.add(new Rock("ROCK MUSIC #1"));
+		musics.add(new Rock("ROCK MUSIC #2"));
+		musics.add(new Rock("ROCK MUSIC #3"));
+		musics.add(new Rock("ROCK MUSIC #4"));
+		musics.add(new Rock("ROCK MUSIC #5"));
 	}
 	
-	public ArrayList<Music> getMusic()
-	{
+	public ArrayList<Music> getMusics()
+	{	
 		return musics;
 	}
-	
+
 }
